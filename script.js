924ee04ddc029a6a0e7bc52e3e35355f886d9d8d
John Helmy <jmhelmy@gmail.com>
2025-04-18 13:46:47 -0700

    Initial commit - Airbnb ROI site

---
+++ script.js
@@ -0,0 +1,14 @@
+fetch('airbnb_filtered.csv')
+  .then(res => res.text())
+  .then(csv => {
+    const [headerLine, ...lines] = csv.trim().split('\n');
+    const headers = headerLine.split(',');
+
+    const thead = document.querySelector('#data-table thead');
+    const tbody = document.querySelector('#data-table tbody');
+
+    thead.innerHTML = '<tr>' + headers.map(h => `<th>${h}</th>`).join('') + '</tr>';
+    tbody.innerHTML = lines.map(row =>
+      '<tr>' + row.split(',').map(cell => `<td>${cell}</td>`).join('') + '</tr>'
+    ).join('');
+  });
